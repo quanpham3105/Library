9284f232490042ac6fd04975fd4b1d563ec328d6
Quan Pham <quanpham3105@gmail.com>
2025-06-14 22:53:37 -0700

    Created project and started on book entity

---
+++ Library/src/main/java/com/QuanWeb/Library/model/Book.java
@@ -0,0 +1,13 @@
+package com.QuanWeb.Library.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Book {
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private int id;
+}
